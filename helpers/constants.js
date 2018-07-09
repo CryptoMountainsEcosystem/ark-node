3d03423a8873fc350923f81a67ede7ffd443b4e3
CryptoMountains <cryptocaru@gmail.com>
2018-07-09 15:08:35 +0200

    prova

--- helpers/constants.js
+++ helpers/constants.js
@@ -8,10 +8,10 @@ module.exports = {
 	confirmationLength: 77,
 	epochTime: new Date(Date.UTC(2017, 2, 21, 13, 0, 0, 0)),
 	fees:{
-		send: 10000000,
-		vote: 100000000,
+		send: 0,
+		vote: 1000000000000,
 		secondsignature: 500000000,
-		delegate: 2500000000,
+		delegate: 1000000000000,
 		multisignature: 500000000
 	},
 	feeStart: 1,
