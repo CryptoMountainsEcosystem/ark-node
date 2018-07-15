de515c642263f81e78e3b6af22433da160634a1f
CryptoMountains <cryptocaru@gmail.com>
2018-07-15 15:49:01 +0200

    CMT-Blockchain

--- helpers/constants.js
+++ helpers/constants.js
@@ -37,10 +37,10 @@ module.exports = {
 			5000000, // Milestone 3
 			5000000  // Milestone 4
 		],
-		offset: 75600,   // Start rewards at block, ie 7 days after net start
+		offset: 1,   // Start rewards at block, ie 7 days after net start
 		distance: 3000000, // Distance between each milestone
 	},
 	signatureLength: 196,
-	totalAmount: 110400000000000000,
+	totalAmount: 100000000000000000,
 	unconfirmedTransactionTimeOut: 10800 // 1080 blocks
 };
